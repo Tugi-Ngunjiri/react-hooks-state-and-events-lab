f6dc20f4c4bf972d288651a90c6cf1b1decdcf9d
your_name <your_email>
2023-01-13 18:12:45 +0300

    completed my assigmet

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,20 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+let isOn =true
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
   const appClass = false ? "App dark" : "App light"
+
+  function handleShoppingListClick(event) {
+    console.log(event.target)
+
+  }
+  function handleClick(event) {
+    isOn = ! isOn 
+    event.target(isOn)
+    console.log(event.target)
+  }
 
   return (
     <div className={appClass}>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -6,6 +6,7 @@ function ShoppingList({ items }) {
     <div className="ShoppingList">
       <div className="Filter">
         <select name="filter">
+       
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -15,10 +16,15 @@ function ShoppingList({ items }) {
       <ul className="Items">
         {items.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        )
+        
+        )}
+       
+
       </ul>
     </div>
-  );
+  )
+  
 }
 
 export default ShoppingList;
